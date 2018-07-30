0572c2ec37e1da6a6944b7b89b3fbc622062cafc
Nathan Kau <nathankau@gmail.com>
2018-07-29 20:25:21 -0700

    indentation fix

--- src/position_control.hpp
+++ src/position_control.hpp
@@ -14,9 +14,10 @@ void EncoderCountsToHipAngle(float enc1, float enc2, float& alpha, float& beta);
 void HipAngleToCartesian(float alpha, float beta, float& x, float& y);
 void LegParamsToHipAngles(float L, float theta, float& alpha, float& beta);
 void LegParamsToCartesian(float L, float theta, float& x, float& y);
-void CartesianToLegParams(float x, float y, float& L, float& theta);
+void CartesianToLegParams(float x, float y, float leg_direction, float& L, float& theta);
 void sinTrajectory(float t, float FREQ, float gaitOffset, float stanceHeight, float flightPercent, float stepLength, float upAMP, float downAMP, float& x, float& y);
-void CartesianToEncoder(float x, float y, float sign, float& enc0, float& enc1);
+void CartesianToEncoder(float x, float y, float leg_direction, float sign, float& enc0, float& enc1);
+void MoveLeg(ODriveArduino& odrive, float t, float FREQ, float gait_offset, float stanceHeight, float flightPercent, float stepLength, float upAMP, float downAMP, float leg_direction, float sign);
 void sinTrajectoryPosControl();
 //------------------------------------------------------------------------------
 // PositionControlThread: Motor position control thread
@@ -38,147 +39,146 @@ static THD_FUNCTION(PositionControlThread, arg) {
 }
 
 /**
- * All the conversions between the different coordinates are given below, we can move them to whatever file we want
- * This needs to define global variable for the Hip angles, leg angles, and foot coordinates so the functions are void
- * We also have to define the upper and lower leg lengths L1 and L2 as global vars
- * Here the Hip angles are assumed to be in radians with a 0 position in the x-direction along the body and positive angles CW
- * Make a HipAngles struct with alpha and beta values
- * The x,y cartesian coordinates have the x-direction pointed where the robot is heading and y-direction is positive towards the ground
- * Make struct with two floats for LegCartesian and give an x,y values
- * The leg length (L) is the virtual leg length and is always positive, the leg angle, theta, is zero in the x-direction and positive when going CW
- * Make a struct with L,theta values called LegParams
- */
+* All the conversions between the different coordinates are given below, we can move them to whatever file we want
+* This needs to define global variable for the Hip angles, leg angles, and foot coordinates so the functions are void
+* We also have to define the upper and lower leg lengths L1 and L2 as global vars
+* Here the Hip angles are assumed to be in radians with a 0 position in the x-direction along the body and positive angles CW
+* Make a HipAngles struct with alpha and beta values
+* The x,y cartesian coordinates have the x-direction pointed where the robot is heading and y-direction is positive towards the ground
+* Make struct with two floats for LegCartesian and give an x,y values
+* The leg length (L) is the virtual leg length and is always positive, the leg angle, theta, is zero in the x-direction and positive when going CW
+* Make a struct with L,theta values called LegParams
+*/
 
 /**
- * Make initial angles for alpha and beta 0 and PI before turning on so that the encoder offset is correctly initialzed.
- * Sign is just 1.0 or -1.0 if the sign of the encoders needs to be flipped
- * Need to change the alpha, beta values as defined above to the encoder counts to set for the position controller. Encoder counts per rev is 2000
- * Make this an if else and handle different orientations, offsets based on which Hip is given in HipIndex
- */
+* Make initial angles for alpha and beta 0 and PI before turning on so that the encoder offset is correctly initialzed.
+* Sign is just 1.0 or -1.0 if the sign of the encoders needs to be flipped
+* Need to change the alpha, beta values as defined above to the encoder counts to set for the position controller. Encoder counts per rev is 2000
+* Make this an if else and handle different orientations, offsets based on which Hip is given in HipIndex
+*/
 void HipAngleToEncoderCounts(float alpha, float beta, float sign, float& enc0, float& enc1) {
-  // have an if else based on if thetaIndex is 0 or 1 for either theta so just return 1 value and avoid matrix
-  const float enc_offset = -PI; // based on initialization
-  const float gear_ratio = 3.0;
-  enc0 = sign * alpha/ (2*PI) * 2000.0 * gear_ratio;
-  enc1 = sign * (beta + enc_offset) / (2*PI) * 2000.0 * gear_ratio;
+    // have an if else based on if thetaIndex is 0 or 1 for either theta so just return 1 value and avoid matrix
+    const float enc_offset = -PI; // based on initialization
+    const float gear_ratio = 3.0;
+    enc0 = sign * alpha/ (2*PI) * 2000.0 * gear_ratio;
+    enc1 = sign * (beta + enc_offset) / (2*PI) * 2000.0 * gear_ratio;
 }
 
 /**
- * TODO: Need to change the encoder counts enc1, enc2 values to the Hip angles as defined above
- * Make this an if else and handle different orientations, offsets based on which Hip is given in HipIndex
- */
+* TODO: Need to change the encoder counts enc1, enc2 values to the Hip angles as defined above
+* Make this an if else and handle different orientations, offsets based on which Hip is given in HipIndex
+*/
 void EncoderCountsToHipAngle(float enc1, float enc2, float& alpha, float& beta) {
 
 }
 
 /**
- * Converts the two Hip angles (in radians) for a leg to the cartesian coordinates
- */
+* Converts the two Hip angles (in radians) for a leg to the cartesian coordinates
+*/
 void HipAngleToCartesian(float alpha, float beta, float& x, float& y) {
-  float L1 = 0.09; // upper leg length (m) what is it actually?
-  float L2 = 0.162; // lower leg length (m)
-  float delta = (beta+alpha+PI)/2.0 - acos(L1/L2*sin((beta-alpha)/2.0));
-  x = L1*cos(alpha) + L2*cos(delta);
-  y = L1*sin(alpha) + L2*sin(delta);
+    float L1 = 0.09; // upper leg length (m) what is it actually?
+    float L2 = 0.162; // lower leg length (m)
+    float delta = (beta+alpha+PI)/2.0 - acos(L1/L2*sin((beta-alpha)/2.0));
+    x = L1*cos(alpha) + L2*cos(delta);
+    y = L1*sin(alpha) + L2*sin(delta);
 }
 
 /**
- * Converts the two Hip angles (in radians) for a leg into the Hip angles alpha, beta (in rads)
- */
+* Converts the two Hip angles (in radians) for a leg into the Hip angles alpha, beta (in rads)
+*/
 void LegParamsToHipAngles(float L, float theta, float& alpha, float& beta) {
-  float L1 = 0.09; // upper leg length (m) what is it actually?
-  float L2 = 0.162; // lower leg length (m)
-  float cos_param = (pow(L1,2.0) + pow(L,2.0) - pow(L2,2.0)) /
-                     (2.0*L1*L);
-  float gamma;
-  if (cos_param < -1.0) {
-    gamma = PI;
-#ifdef DEBUG_HIGH
-    Serial.println("ERROR: L is too small to find valid alpha and beta!");
-#endif
-  } else if (cos_param > 1.0) {
-    gamma = 0;
-#ifdef DEBUG_HIGH
-    Serial.println("ERROR: L is too large to find valid alpha and beta!");
-#endif
-  } else {
-    gamma = acos(cos_param);
-  }
-  alpha = theta - gamma;
-  beta = theta + gamma;
+    float L1 = 0.09; // upper leg length (m) what is it actually?
+    float L2 = 0.162; // lower leg length (m)
+    float cos_param = (pow(L1,2.0) + pow(L,2.0) - pow(L2,2.0)) / (2.0*L1*L);
+    float gamma;
+    if (cos_param < -1.0) {
+        gamma = PI;
+        #ifdef DEBUG_HIGH
+        Serial.println("ERROR: L is too small to find valid alpha and beta!");
+        #endif
+    } else if (cos_param > 1.0) {
+        gamma = 0;
+        #ifdef DEBUG_HIGH
+        Serial.println("ERROR: L is too large to find valid alpha and beta!");
+        #endif
+    } else {
+        gamma = acos(cos_param);
+    }
+    alpha = theta - gamma;
+    beta = theta + gamma;
 }
 
 /**
- * Converts the leg params L, gamma to cartesian coordinates x, y (in m)
- * Set x_direction to 1.0 or -1.0 to change which direction the leg walks
- */
+* Converts the leg params L, gamma to cartesian coordinates x, y (in m)
+* Set x_direction to 1.0 or -1.0 to change which direction the leg walks
+*/
 void LegParamsToCartesian(float L, float theta, float leg_direction, float& x, float& y) {
-  x = leg_direction * L * cos(theta);
-  y = L * sin(theta);
+    x = leg_direction * L * cos(theta);
+    y = L * sin(theta);
 }
 
 /**
- * Converts the cartesian coords x, y (m) to leg params L (m), gamma (rad)
- */
+* Converts the cartesian coords x, y (m) to leg params L (m), gamma (rad)
+*/
 void CartesianToLegParams(float x, float y, float leg_direction, float& L, float& theta) {
-  L = pow(
-        (pow(x,2.0) + pow(y,2.0))
-        ,0.5);
-  theta = atan2(y,leg_direction * x);
+    L = pow( (pow(x,2.0) + pow(y,2.0)) ,0.5);
+    theta = atan2(y,leg_direction * x);
 }
 
 /**
- * Sinusoidal trajectory generator function with flexibility from parameters described below. Can do 4-beat, 2-beat, trotting, etc with this.
- */
-void sinTrajectory(float t, float FREQ, float gaitOffset, float stanceHeight, float flightPercent, float stepLength, float upAMP, float downAMP, float& x, float& y) {
+* Sinusoidal trajectory generator function with flexibility from parameters described below. Can do 4-beat, 2-beat, trotting, etc with this.
+*/
+void sinTrajectory (float t, float FREQ, float gaitOffset, float stanceHeight,
+                    float flightPercent, float stepLength, float upAMP,
+                    float downAMP, float& x, float& y) {
     float gp = fmod((FREQ*t+gaitOffset),1.0); // mod(a,m) returns remainder division of a by m
     if (gp <= flightPercent) {
-      x = (gp/flightPercent)*stepLength - stepLength/2.0;
-      y = -upAMP*sin(PI*gp/flightPercent) + stanceHeight;
+        x = (gp/flightPercent)*stepLength - stepLength/2.0;
+        y = -upAMP*sin(PI*gp/flightPercent) + stanceHeight;
     }
     else {
-      float percentBack = (gp-flightPercent)/(1.0-flightPercent);
-      x = -percentBack*stepLength + stepLength/2.0;
-      y = downAMP*sin(PI*percentBack) + stanceHeight;
+        float percentBack = (gp-flightPercent)/(1.0-flightPercent);
+        x = -percentBack*stepLength + stepLength/2.0;
+        y = downAMP*sin(PI*percentBack) + stanceHeight;
     }
 }
 
-void CartesianToEncoder(float x, float y, float leg_direction, float enc_sign, float& enc0, float& enc1){
-  float L = 0.0;
-  float theta = 0.0;
-  CartesianToLegParams(x, y, leg_direction, L, theta);
-  float alpha = 0.0;
-  float beta = 0.0;
-  LegParamsToHipAngles(L, theta, alpha, beta);
-  HipAngleToEncoderCounts(alpha, beta, enc_sign, enc0, enc1);
+void CartesianToEncoder(float x, float y, float leg_direction, float enc_sign,
+                        float& enc0, float& enc1){
+    float L = 0.0;
+    float theta = 0.0;
+    CartesianToLegParams(x, y, leg_direction, L, theta);
+    float alpha = 0.0;
+    float beta = 0.0;
+    LegParamsToHipAngles(L, theta, alpha, beta);
+    HipAngleToEncoderCounts(alpha, beta, enc_sign, enc0, enc1);
 
-  Serial.print(x,4);
-  Serial.print(" ");
-  Serial.print(y,4);
-  Serial.print(" ");
-  Serial.print(L);
-  Serial.print(" ");
-  Serial.print(theta);
-  Serial.println();
+    Serial.print(x,4);
+    Serial.print(" ");
+    Serial.print(y,4);
+    Serial.print(" ");
+    Serial.print(L);
+    Serial.print(" ");
+    Serial.print(theta);
+    Serial.println();
 }
 
-void MoveLeg(ODriveArduino& odrive,
-            float t, float FREQ, float gait_offset, float stanceHeight,
-            float flightPercent, float stepLength, float upAMP, float downAMP,
-            float leg_direction, float sign) {
-  float enc0;
-  float enc1;
-  float x; // float x for leg 0 to be set by the sin trajectory
-  float y;
-  sinTrajectory(t, FREQ, gait_offset, stanceHeight, flightPercent, stepLength, upAMP, downAMP, x, y);
-  CartesianToEncoder(x, y, leg_direction, sign, enc0, enc1);
-  odrive.SetPosition(0,(int)enc0);
-  odrive.SetPosition(1,(int)enc1);
+void MoveLeg(ODriveArduino& odrive, float t, float FREQ, float gait_offset,
+             float stanceHeight, float flightPercent, float stepLength,
+             float upAMP, float downAMP, float leg_direction, float sign) {
+    float enc0;
+    float enc1;
+    float x; // float x for leg 0 to be set by the sin trajectory
+    float y;
+    sinTrajectory(t, FREQ, gait_offset, stanceHeight, flightPercent, stepLength, upAMP, downAMP, x, y);
+    CartesianToEncoder(x, y, leg_direction, sign, enc0, enc1);
+    odrive.SetPosition(0,(int)enc0);
+    odrive.SetPosition(1,(int)enc1);
 }
 
 /**
- * Drives the ODrives in an open-loop, position-control sinTrajectory.
- */
+* Drives the ODrives in an open-loop, position-control sinTrajectory.
+*/
 void sinTrajectoryPosControl() {
     // min radius = 0.8
     // max radius = 0.25
@@ -195,34 +195,34 @@ void sinTrajectoryPosControl() {
     const float leg0_sign = -1.0;
     const float leg0_direction = -1.0;
     MoveLeg(odrv0Interface, t, FREQ, leg0_offset, stanceHeight,
-            flightPercent, stepLength, upAMP, downAMP,
-            leg0_direction, leg0_sign);
+        flightPercent, stepLength, upAMP, downAMP,
+        leg0_direction, leg0_sign);
 
     const float leg1_offset = 0.25;
     const float leg1_sign = -1.0;
     const float leg1_direction = -1.0;
     MoveLeg(odrv1Interface, t, FREQ, leg1_offset, stanceHeight,
-            flightPercent, stepLength, upAMP, downAMP,
-            leg1_direction, leg1_sign);
+        flightPercent, stepLength, upAMP, downAMP,
+        leg1_direction, leg1_sign);
 
     const float leg2_offset = 0.75;
     const float leg2_sign = -1.0;
     const float leg2_direction = 1.0;
     MoveLeg(odrv2Interface, t, FREQ, leg2_offset, stanceHeight,
-            flightPercent, stepLength, upAMP, downAMP,
-            leg2_direction, leg2_sign);
+        flightPercent, stepLength, upAMP, downAMP,
+        leg2_direction, leg2_sign);
 
     const float leg3_offset = 0.5;
     const float leg3_sign = -1.0;
     const float leg3_direction = 1.0;
     MoveLeg(odrv3Interface, t, FREQ, leg3_offset, stanceHeight,
-            flightPercent, stepLength, upAMP, downAMP,
-            leg3_direction, leg3_sign);
+        flightPercent, stepLength, upAMP, downAMP,
+        leg3_direction, leg3_sign);
 }
 
 /**
- * Drives the ODrives in an open-loop, position-control trajectory.
- */
+* Drives the ODrives in an open-loop, position-control trajectory.
+*/
 void ODrivePosControl() {
 
     float FREQ = 1.0; // hz
@@ -261,9 +261,9 @@ void ODrivePosControl() {
 }
 
 /**
- * Computes PID onboard the teensy to enable coupled control on the legs.
- * Right now it only computes control for a single leg.
- */
+* Computes PID onboard the teensy to enable coupled control on the legs.
+* Right now it only computes control for a single leg.
+*/
 void CoupledPIDControl() {
     float alpha = (float) odrv0.axis0.abs_pos_estimate;
     float beta = (float) odrv0.axis1.abs_pos_estimate;
@@ -291,18 +291,18 @@ void CoupledPIDControl() {
 
     latest_send_timestamp = micros();
 
-#ifdef DEBUG_HIGH
+    #ifdef DEBUG_HIGH
     Serial << "Sending at: " << micros() << '\n';
-#endif
+    #endif
     // DEBUG only: send two zero current commands
     odrv0Interface.SetDualCurrent(0, 0);
-#ifdef DEBUG_HIGH
+    #ifdef DEBUG_HIGH
     Serial << "Sent at: " << micros() << '\n';
-#endif
+    #endif
 
-#ifdef PRINT_ONCE
+    #ifdef PRINT_ONCE
     chThdSleepMilliseconds(1000000);
-#endif
+    #endif
     // The duration of sleep controls the loop frequency
     chThdSleepMicroseconds(1000000/POSITION_CONTROL_FREQ);
 }
