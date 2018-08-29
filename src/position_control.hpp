c45fc8fada617d34e8fc0d6d6a203b5ee8307edf
Nathan Kau <nathankau@gmail.com>
2018-08-28 21:34:50 -0700

    Merging Patricks gait control code with coupled pd control

--- src/position_control.hpp
+++ src/position_control.hpp
@@ -8,20 +8,19 @@
 #define POSITION_CONTROL_H
 
 void CoupledPIDControl();
-void ODrivePosControl();
-void HipAngleToEncoderCounts(float alpha, float beta, float& enc1, float& enc2);
-void EncoderCountsToHipAngle(float enc1, float enc2, float& alpha, float& beta);
 void HipAngleToCartesian(float alpha, float beta, float& x, float& y);
-void getGamma(float L, float theta, float& gamma);
+void GetGamma(float L, float theta, float& gamma);
 void LegParamsToHipAngles(float L, float theta, float& alpha, float& beta);
 void LegParamsToCartesian(float L, float theta, float& x, float& y);
 void CartesianToLegParams(float x, float y, float leg_direction, float& L, float& theta);
 void CartesianToThetaGamma(float x, float y, float leg_direction, float& theta, float& gamma);
-void sinTrajectory(float t, float FREQ, float gaitOffset, float stanceHeight, float flightPercent, float stepLength, float upAMP, float downAMP, float& x, float& y);
+void SinTrajectory(float t, float FREQ, float gaitOffset, float stanceHeight, float flightPercent, float stepLength, float upAMP, float downAMP, float& x, float& y);
 void CartesianToEncoder(float x, float y, float leg_direction, float sign, float& enc0, float& enc1);
-void MoveLeg(ODriveArduino& odrive, float t, float FREQ, float gait_offset, float stanceHeight, float flightPercent, float stepLength, float upAMP, float downAMP, float leg_direction, float sign);
-void CoupledMoveLeg(ODriveArduino& odrive, float t, float FREQ, float gait_offset, float stanceHeight, float flightPercent, float stepLength, float upAMP, float downAMP, float leg_direction, float sign);
-void sinTrajectoryPosControl();
+void CoupledMoveLeg(ODriveArduino& odrive, float t, float FREQ, float gait_offset, float stanceHeight, float flightPercent, float stepLength, float upAMP, float downAMP, float leg_direction);
+void SinTrajectoryPosControl();
+void trot();
+void pronk();
+void bound();
 //------------------------------------------------------------------------------
 // PositionControlThread: Motor position control thread
 // Periodically calculates result from PID controller and sends off new motor
@@ -38,7 +37,6 @@ static THD_FUNCTION(PositionControlThread, arg) {
         // CoupledPIDControl();
         // ODrivePosControl();
         // sinTrajectoryPosControl();
-        //odrv1Interface.SetCoupledPosition(PI / 4.0 * sin(millis() /2000.0 * PI) + PI / 2.0, PI / 4.0 * sin(millis() /2000.0 * PI+PI/2.0)+PI/3.0);
 
         const float stanceHeight = 0.15; // Desired height of body from ground during walking (m)
         const float downAMP = 0.0; // Peak amplitude below stanceHeight in sinusoidal trajectory (m)
@@ -50,49 +48,31 @@ static THD_FUNCTION(PositionControlThread, arg) {
         float t = millis()/1000.0;
 
         const float leg1_offset = 0.25;
-        const float leg1_sign = 1.0;
         const float leg1_direction = 1.0;
         CoupledMoveLeg(odrv1Interface, t, FREQ, leg1_offset, stanceHeight,
             flightPercent, stepLength, upAMP, downAMP,
-            leg1_direction, leg1_sign);
+            leg1_direction);
 
         chThdSleepMicroseconds(1000000/POSITION_CONTROL_FREQ);
     }
 }
 
 /**
-* All the conversions between the different coordinates are given below, we can move them to whatever file we want
-* This needs to define global variable for the Hip angles, leg angles, and foot coordinates so the functions are void
+* All the conversions between the different coordinates are given below, we can move them to whatever file we want.
+* This needs to define global variable for the Hip angles, leg angles, and foot coordinates so the functions are void.
 * We also have to define the upper and lower leg lengths L1 and L2 as global vars
-* Here the Hip angles are assumed to be in radians with a 0 position in the x-direction along the body and positive angles CW
-* Make a HipAngles struct with alpha and beta values
-* The x,y cartesian coordinates have the x-direction pointed where the robot is heading and y-direction is positive towards the ground
-* Make struct with two floats for LegCartesian and give an x,y values
-* The leg length (L) is the virtual leg length and is always positive, the leg angle, theta, is zero in the x-direction and positive when going CW
-* Make a struct with L,theta values called LegParams
+* Here the Hip angles are assumed to be in radians with a 0 position in the
+* downward y-direction and positive angles CCW.
+* TODO: Make a HipAngles struct with alpha and beta values
+* The x,y cartesian coordinates have the x-direction pointed where the robot is
+* heading and y-direction is positive towards the ground
+* TODO: Make struct with two floats for LegCartesian and give an x,y values
+* The leg length (L) is the virtual leg length and is always positive,
+* the leg angle, theta, is zero in the downward y-direction and positive when
+* going CWW.
+* TODO: Make a struct with L,theta values called LegParams
 */
 
-/**
-* Make initial angles for alpha and beta 0 and PI before turning on so that the encoder offset is correctly initialzed.
-* Sign is just 1.0 or -1.0 if the sign of the encoders needs to be flipped
-* Need to change the alpha, beta values as defined above to the encoder counts to set for the position controller. Encoder counts per rev is 2000
-* Make this an if else and handle different orientations, offsets based on which Hip is given in HipIndex
-*/
-void HipAngleToEncoderCounts(float alpha, float beta, float sign, float& enc0, float& enc1) {
-    // have an if else based on if thetaIndex is 0 or 1 for either theta so just return 1 value and avoid matrix
-    const float enc_offset = -PI; // based on initialization
-    const float gear_ratio = 3.0;
-    enc0 = sign * alpha/ (2*PI) * 2000.0 * gear_ratio;
-    enc1 = sign * (beta + enc_offset) / (2*PI) * 2000.0 * gear_ratio;
-}
-
-/**
-* TODO: Need to change the encoder counts enc1, enc2 values to the Hip angles as defined above
-* Make this an if else and handle different orientations, offsets based on which Hip is given in HipIndex
-*/
-void EncoderCountsToHipAngle(float enc1, float enc2, float& alpha, float& beta) {
-
-}
 
 /**
 * Converts the two Hip angles (in radians) for a leg to the cartesian coordinates
@@ -108,7 +88,7 @@ void HipAngleToCartesian(float alpha, float beta, float& x, float& y) {
 /**
 * Takes the leg parameters and returns the gamma angle (rad) of the legs
 */
-void getGamma(float L, float theta, float& gamma) {
+void GetGamma(float L, float theta, float& gamma) {
     float L1 = 0.09; // upper leg length (m) what is it actually?
     float L2 = 0.162; // lower leg length (m)
     float cos_param = (pow(L1,2.0) + pow(L,2.0) - pow(L2,2.0)) / (2.0*L1*L);
@@ -132,7 +112,7 @@ void getGamma(float L, float theta, float& gamma) {
 */
 void LegParamsToHipAngles(float L, float theta, float& alpha, float& beta) {
     float gamma;
-    getGamma(L, theta, gamma);
+    GetGamma(L, theta, gamma);
     alpha = theta - gamma;
     beta = theta + gamma;
 }
@@ -157,7 +137,7 @@ void CartesianToLegParams(float x, float y, float leg_direction, float& L, float
 /**
 * Sinusoidal trajectory generator function with flexibility from parameters described below. Can do 4-beat, 2-beat, trotting, etc with this.
 */
-void sinTrajectory (float t, float FREQ, float gaitOffset, float stanceHeight,
+void SinTrajectory (float t, float FREQ, float gaitOffset, float stanceHeight,
                     float flightPercent, float stepLength, float upAMP,
                     float downAMP, float& x, float& y) {
     float gp = fmod((FREQ*t+gaitOffset),1.0); // mod(a,m) returns remainder division of a by m
@@ -172,194 +152,155 @@ void sinTrajectory (float t, float FREQ, float gaitOffset, float stanceHeight,
     }
 }
 
-void CartesianToEncoder(float x, float y, float leg_direction, float enc_sign,
-                        float& enc0, float& enc1){
-    float L = 0.0;
-    float theta = 0.0;
-    CartesianToLegParams(x, y, leg_direction, L, theta);
-    float alpha = 0.0;
-    float beta = 0.0;
-    LegParamsToHipAngles(L, theta, alpha, beta);
-    HipAngleToEncoderCounts(alpha, beta, enc_sign, enc0, enc1);
-
-    Serial.print(x,4);
-    Serial.print(" ");
-    Serial.print(y,4);
-    Serial.print(" ");
-    // Serial.print(L);
-    // Serial.print(" ");
-    // Serial.print(theta);
-    // Serial.println();
-}
-
 void CartesianToThetaGamma(float x, float y, float leg_direction, float& theta, float& gamma) {
     float L = 0.0;
     CartesianToLegParams(x, y, leg_direction, L, theta);
-    getGamma(L, theta, gamma);
+    GetGamma(L, theta, gamma);
     Serial.print(theta);
     Serial.print(" ");
     Serial.print(gamma);
     Serial.print('\n');
 }
 
-void MoveLeg(ODriveArduino& odrive, float t, float FREQ, float gait_offset,
-             float stanceHeight, float flightPercent, float stepLength,
-             float upAMP, float downAMP, float leg_direction, float sign) {
-    float enc0;
-    float enc1;
-    float x; // float x for leg 0 to be set by the sin trajectory
-    float y;
-    sinTrajectory(t, FREQ, gait_offset, stanceHeight, flightPercent, stepLength, upAMP, downAMP, x, y);
-    CartesianToEncoder(x, y, leg_direction, sign, enc0, enc1);
-    odrive.SetPosition(0,(int)enc0);
-    odrive.SetPosition(1,(int)enc1);
-}
-
 void CoupledMoveLeg(ODriveArduino& odrive, float t, float FREQ, float gait_offset,
              float stanceHeight, float flightPercent, float stepLength,
-             float upAMP, float downAMP, float leg_direction, float sign) {
+             float upAMP, float downAMP, float leg_direction) {
     float theta;
     float gamma;
     float x; // float x for leg 0 to be set by the sin trajectory
     float y;
-    sinTrajectory(t, FREQ, gait_offset, stanceHeight, flightPercent, stepLength, upAMP, downAMP, x, y);
+    SinTrajectory(t, FREQ, gait_offset, stanceHeight, flightPercent, stepLength, upAMP, downAMP, x, y);
     CartesianToThetaGamma(x, y, leg_direction, theta, gamma);
     odrive.SetCoupledPosition(theta, 38.2, 0.48, gamma, 20, 0.48);
 }
 
 /**
-* Drives the ODrives in an open-loop, position-control sinTrajectory.
+* Pronk gait parameters
 */
-void sinTrajectoryPosControl() {
+void pronk() {
     // min radius = 0.8
     // max radius = 0.25
     const float stanceHeight = 0.15; // Desired height of body from ground during walking (m)
-    const float downAMP = 0.0; // Peak amplitude below stanceHeight in sinusoidal trajectory (m)
-    const float upAMP = 0.07; // Height the foot peaks at above the stanceHeight in sinusoidal trajectory (m)
-    const float flightPercent = 0.2; // Portion of the gait time should be doing the down portion of trajectory
-    const float stepLength = 0.12; // Length of entire step (m)
+    const float downAMP = 0.05; // Peak amplitude below stanceHeight in sinusoidal trajectory (m)
+    const float upAMP = 0.05; // Height the foot peaks at above the stanceHeight in sinusoidal trajectory (m)
+    const float flightPercent = 0.4; // Portion of the gait time should be doing the down portion of trajectory
+    const float stepLength = 0.0; //0.12; // Length of entire step (m)
     const float FREQ = 1.0; // Frequency of one gait cycle (Hz)
-    const float gaitOffset1 = 0.0; // Phase shift in percent (i.e. 25% shift is 0.25) to be passed in depending on Hip
     float t = millis()/1000.0;
 
     Serial.print(t);
     Serial.print(" ");
 
     const float leg0_offset = 0.0;
-    const float leg0_sign = -1.0;
     const float leg0_direction = -1.0;
-    MoveLeg(odrv0Interface, t, FREQ, leg0_offset, stanceHeight,
+    CoupledMoveLeg(odrv0Interface, t, FREQ, leg0_offset, stanceHeight,
         flightPercent, stepLength, upAMP, downAMP,
-        leg0_direction, leg0_sign);
+        leg0_direction);
 
-    const float leg1_offset = 0.25;
-    const float leg1_sign = -1.0;
+    const float leg1_offset = 0.0;
     const float leg1_direction = -1.0;
-    MoveLeg(odrv1Interface, t, FREQ, leg1_offset, stanceHeight,
+    CoupledMoveLeg(odrv1Interface, t, FREQ, leg1_offset, stanceHeight,
         flightPercent, stepLength, upAMP, downAMP,
-        leg1_direction, leg1_sign);
+        leg1_direction);
 
-    const float leg2_offset = 0.75;
-    const float leg2_sign = -1.0;
+    const float leg2_offset = 0.0;
     const float leg2_direction = 1.0;
-    MoveLeg(odrv2Interface, t, FREQ, leg2_offset, stanceHeight,
+    CoupledMoveLeg(odrv2Interface, t, FREQ, leg2_offset, stanceHeight,
         flightPercent, stepLength, upAMP, downAMP,
-        leg2_direction, leg2_sign);
+        leg2_direction);
 
-    const float leg3_offset = 0.5;
-    const float leg3_sign = -1.0;
+    const float leg3_offset = 0.0;
     const float leg3_direction = 1.0;
-    MoveLeg(odrv3Interface, t, FREQ, leg3_offset, stanceHeight,
+    CoupledMoveLeg(odrv3Interface, t, FREQ, leg3_offset, stanceHeight,
         flightPercent, stepLength, upAMP, downAMP,
-        leg3_direction, leg3_sign);
+        leg3_direction);
     Serial.println();
 }
 
 /**
-* Drives the ODrives in an open-loop, position-control trajectory.
+* Trot gait parameters
 */
-void ODrivePosControl() {
+void trot() {
+    // min radius = 0.8
+    // max radius = 0.25
+    const float stanceHeight = 0.15; // Desired height of body from ground during walking (m)
+    const float downAMP = 0.0; // Peak amplitude below stanceHeight in sinusoidal trajectory (m)
+    const float upAMP = 0.05; // Height the foot peaks at above the stanceHeight in sinusoidal trajectory (m)
+    const float flightPercent = 0.35; // Portion of the gait time should be doing the down portion of trajectory
+    const float stepLength = 0.0; // Length of entire step (m)
+    const float FREQ = 1.0; // Frequency of one gait cycle (Hz)
+    float t = millis()/1000.0;
 
-    float FREQ = 1.0; // hz
-    float AMP = 800.0;
-    float t = millis()/1000.0 * FREQ;
+    Serial.print(t);
+    Serial.print(" ");
 
-    // Leg 0
-    float sp00 = AMP*sin(2*PI*t);
-    float sp01 = AMP*sin(2*PI*t + PI/2.0);
+    const float leg0_offset = 0.0;
+    const float leg0_direction = -1.0;
+    CoupledMoveLeg(odrv0Interface, t, FREQ, leg0_offset, stanceHeight,
+        flightPercent, stepLength, upAMP, downAMP,
+        leg0_direction);
 
-    // Leg 1
-    float leg1_phase_shift = PI/2.0;
-    float sp10 = AMP*sin(2*PI*t + leg1_phase_shift);
-    float sp11 = AMP*sin(2*PI*t + leg1_phase_shift + PI/2.0);
+    const float leg1_offset = 0.5;
+    const float leg1_direction = -1.0;
+    CoupledMoveLeg(odrv1Interface, t, FREQ, leg1_offset, stanceHeight,
+        flightPercent, stepLength, upAMP, downAMP,
+        leg1_direction);
 
-    // Leg 2
-    const float leg2_phase_shift = PI + PI;
-    float sp30 = AMP*sin(2*PI*t + PI/2.0 + leg2_phase_shift);
-    float sp31 = AMP*sin(2*PI*t + leg2_phase_shift);
+    const float leg2_offset = 0.5;
+    const float leg2_direction = 1.0;
+    CoupledMoveLeg(odrv2Interface, t, FREQ, leg2_offset, stanceHeight,
+        flightPercent, stepLength, upAMP, downAMP,
+        leg2_direction);
 
-    // Leg 3
-    const float leg3_phase_shift = PI/2.0;
-    float sp20 = AMP*sin(2*PI*t + PI/2.0 + leg3_phase_shift);
-    float sp21 = AMP*sin(2*PI*t + leg3_phase_shift);
-
-    odrv0Interface.SetPosition(0,sp00);
-    odrv0Interface.SetPosition(1,sp01);
-    odrv1Interface.SetPosition(0,sp10);
-    odrv1Interface.SetPosition(1,sp11);
-    odrv2Interface.SetPosition(0,sp20);
-    odrv2Interface.SetPosition(1,sp21);
-    odrv3Interface.SetPosition(0,sp30);
-    odrv3Interface.SetPosition(1,sp31);
-
-    chThdSleepMilliseconds(5);
+    const float leg3_offset = 0.0;
+    const float leg3_direction = 1.0;
+    CoupledMoveLeg(odrv3Interface, t, FREQ, leg3_offset, stanceHeight,
+        flightPercent, stepLength, upAMP, downAMP,
+        leg3_direction);
+    Serial.println();
 }
 
 /**
-* Computes PID onboard the teensy to enable coupled control on the legs.
-* Right now it only computes control for a single leg.
+* Bound gait parameters
 */
-void CoupledPIDControl() {
-    float alpha = (float) odrv0.axis0.abs_pos_estimate;
-    float beta = (float) odrv0.axis1.abs_pos_estimate;
+void bound() {
+    // min radius = 0.8
+    // max radius = 0.25
+    const float stanceHeight = 0.15; // Desired height of body from ground during walking (m)
+    const float downAMP = 0.4; // Peak amplitude below stanceHeight in sinusoidal trajectory (m)
+    const float upAMP = 0.04; // Height the foot peaks at above the stanceHeight in sinusoidal trajectory (m)
+    const float flightPercent = 0.5; // Portion of the gait time should be doing the down portion of trajectory
+    const float stepLength = 0.0; // Length of entire step (m)
+    const float FREQ = 1.0; // Frequency of one gait cycle (Hz)
+    float t = millis()/1000.0;
 
-    float theta = alpha/2.0 + beta/2.0;
-    float gamma = beta/2.0 - alpha/2.0;
+    Serial.print(t);
+    Serial.print(" ");
 
-    float theta_sp = 0; // TODO take as struct or something
-    float gamma_sp = 0; // TODO take as struct or something
+    const float leg0_offset = 0.0;
+    const float leg0_direction = -1.0;
+    CoupledMoveLeg(odrv0Interface, t, FREQ, leg0_offset, stanceHeight,
+        flightPercent, stepLength, upAMP, downAMP,
+        leg0_direction);
 
-    float p_term_theta = leg_default.Kp_theta * (theta_sp - theta);
-    float d_term_theta = leg_default.Kd_theta * (0); // TODO: Add motor velocities to position message from odrive
+    const float leg1_offset = 0.5;
+    const float leg1_direction = -1.0;
+    CoupledMoveLeg(odrv1Interface, t, FREQ, leg1_offset, stanceHeight,
+        flightPercent, stepLength, upAMP, downAMP,
+        leg1_direction);
 
-    float p_term_gamma = leg_default.Kp_gamma * (gamma_sp - gamma);
-    float d_term_gamma = leg_default.Kd_gamma * (0); // TODO: Add motor velocities to position message from odrive
+    const float leg2_offset = 0.0;
+    const float leg2_direction = 1.0;
+    CoupledMoveLeg(odrv2Interface, t, FREQ, leg2_offset, stanceHeight,
+        flightPercent, stepLength, upAMP, downAMP,
+        leg2_direction);
 
-    // TODO: clamp (ie constrain) the outputs to -1.0 to 1.0
-    float tau_theta = p_term_theta + d_term_theta;
-    float tau_gamma = p_term_gamma + d_term_gamma;
-
-    // TODO: check signs
-    float tau_alpha = tau_theta*0.5 - tau_gamma*0.5;
-    float tau_beta = tau_theta*0.5 + tau_gamma*0.5;
-    // odrv0Interface.SetDualCurrent(tau_alpha, tau_gamma);
-
-    latest_send_timestamp = micros();
-
-    #ifdef DEBUG_HIGH
-    Serial << "Sending at: " << micros() << '\n';
-    #endif
-    // DEBUG only: send two zero current commands
-    odrv0Interface.SetDualCurrent(0, 0);
-    #ifdef DEBUG_HIGH
-    Serial << "Sent at: " << micros() << '\n';
-    #endif
-
-    #ifdef PRINT_ONCE
-    chThdSleepMilliseconds(1000000);
-    #endif
-    // The duration of sleep controls the loop frequency
-    chThdSleepMicroseconds(1000000/POSITION_CONTROL_FREQ);
+    const float leg3_offset = 0.5;
+    const float leg3_direction = 1.0;
+    CoupledMoveLeg(odrv3Interface, t, FREQ, leg3_offset, stanceHeight,
+        flightPercent, stepLength, upAMP, downAMP,
+        leg3_direction);
+    Serial.println();
 }
 
 #endif
