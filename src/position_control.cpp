ea02a174a02a8974faa4a727d43ead97f552cea2
Aaron Schultz <aaron@hschultz.net>
2018-09-05 19:30:37 -0700

    Reverted style of gait()

--- src/position_control.cpp
+++ src/position_control.cpp
@@ -247,27 +247,27 @@ void gait(struct GaitParams params,
                 float leg2_offset, float leg3_offset,
                 struct LegGain gains) {
 
-    if (IsValidGaitParams(params) && IsValidLegGain(gains)) {
-        float t = millis()/1000.0;
-
-        const float leg0_direction = -1.0;
-        CoupledMoveLeg(odrv0Interface, t, params, leg0_offset, leg0_direction, gains,
-            global_debug_values.odrv0.sp_theta, global_debug_values.odrv0.sp_gamma);
-
-        const float leg1_direction = -1.0;
-        CoupledMoveLeg(odrv1Interface, t, params, leg1_offset, leg1_direction, gains,
-            global_debug_values.odrv1.sp_theta, global_debug_values.odrv1.sp_gamma);
-
-        const float leg2_direction = 1.0;
-        CoupledMoveLeg(odrv2Interface, t, params, leg2_offset, leg2_direction, gains,
-            global_debug_values.odrv2.sp_theta, global_debug_values.odrv2.sp_gamma);
-
-        const float leg3_direction = 1.0;
-        CoupledMoveLeg(odrv3Interface, t, params, leg3_offset, leg3_direction, gains,
-            global_debug_values.odrv3.sp_theta, global_debug_values.odrv3.sp_gamma);
-    } else {
-        return; // gait function will not run with invalid parameters
+    if (!IsValidGaitParams(params) || !IsValidLegGain(gains)) {
+        return;
     }
+
+    float t = millis()/1000.0;
+
+    const float leg0_direction = -1.0;
+    CoupledMoveLeg(odrv0Interface, t, params, leg0_offset, leg0_direction, gains,
+        global_debug_values.odrv0.sp_theta, global_debug_values.odrv0.sp_gamma);
+
+    const float leg1_direction = -1.0;
+    CoupledMoveLeg(odrv1Interface, t, params, leg1_offset, leg1_direction, gains,
+        global_debug_values.odrv1.sp_theta, global_debug_values.odrv1.sp_gamma);
+
+    const float leg2_direction = 1.0;
+    CoupledMoveLeg(odrv2Interface, t, params, leg2_offset, leg2_direction, gains,
+        global_debug_values.odrv2.sp_theta, global_debug_values.odrv2.sp_gamma);
+
+    const float leg3_direction = 1.0;
+    CoupledMoveLeg(odrv3Interface, t, params, leg3_offset, leg3_direction, gains,
+        global_debug_values.odrv3.sp_theta, global_debug_values.odrv3.sp_gamma);
 }
 
 
