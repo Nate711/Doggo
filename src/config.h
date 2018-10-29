d9c1f6a882745a46c8f8bbd926ce412b227df2bd
Nathan Kau <nathankau@gmail.com>
2018-10-29 13:55:30 -0700

    Rearranged config file

--- src/config.h
+++ src/config.h
@@ -1,25 +1,14 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-// Define USE_XBEE to cause all debug prints to go through the xbee
-// Comment out the line to debug over usb
-// #define USE_XBEE
-
-
 // Define DEBUG_LOW and/or DEBUG_HIGH to enable printing debug messages
 // I've made two flags so you have more options in terms of which messages to show
-//#define DEBUG_HIGH
-//#define DEBUG_LOW
+// #define DEBUG_HIGH
+// #define DEBUG_LOW
 // #define PRINT_ONCE
 
-// Setting this value above 0 will cause the imu thread to print debug messages
-#define IMU_VERBOSE 1
-
-// Set to 1 to enable data logging, set to 0 to disable
-#define ENABLE_DATALOGGER 0
-#define DATALOGGER_VERBOSE 0
-
-// Important: Thread execution frequencies
+//------------------------------------------------------------------------------
+// Thread execution rates
 #define POSITION_CONTROL_FREQ 100
 #define DEBUG_PRINT_FREQ 20
 #define UART_FREQ 2000
@@ -28,15 +17,20 @@
 #define IMU_FREQ 400
 #define IMU_SEND_FREQ 100
 
+//------------------------------------------------------------------------------
+// Robot Safety Parameters
 #define CURRENT_LIM 40.0f
 
 //------------------------------------------------------------------------------
-// Accessories (not including ODRives)
+// XBEE Config
+// Define USE_XBEE to cause all debug prints to go through the xbee
+// Comment out the line to debug over usb
+// #define USE_XBEE
 
-#define SPI_CS_PIN 10
-#define SPI_WAK_PIN 9
-#define SPI_INTPIN 7
-#define SPI_RSTPIN 8
+// Xbee serial
+#ifdef USE_XBEE
+#define Serial Serial5
+#endif
 
 //------------------------------------------------------------------------------
 // IMU Parameters
@@ -44,10 +38,21 @@
 #define IMU_ENABLE_COMPLEMENTARY_FILTER 1
 #define IMU_COMPLEMENTARY_FILTER_TAU 0.99f
 
+// Set above 0 to print imu debug messages
+#define IMU_VERBOSE 1
+
+// Pins for BNO080 IMU
+#define SPI_CS_PIN 10
+#define SPI_WAK_PIN 9
+#define SPI_INTPIN 7
+#define SPI_RSTPIN 8
+
 //------------------------------------------------------------------------------
-// Xbee serial
-#ifdef USE_XBEE
-#define Serial Serial5
-#endif
+// Datalogger parameters
+// Set to 1 to enable data logging, set to 0 to disable
+#define ENABLE_DATALOGGER 0
+
+// Set above 0 to print debugging messages
+#define DATALOGGER_VERBOSE 0
 
 #endif
