93cd710efbf00260337f4b7c388fec5162a8aa7d
Aaron Schultz <aaron@hschultz.net>
2018-11-11 19:10:31 -0800

    Working towards full backflip

--- src/backflip.cpp
+++ src/backflip.cpp
@@ -24,25 +24,28 @@ void StartFlip(float start_time_s) {
     Serial.println("FLIP");
     flip_start_time_ = start_time_s;
     //            {s.h, d.a., u.a., f.p., s.l., fr.}
-    gait_params = {0.15, 0.07, 0.06, 0.2, 0, 1.0};
+    gait_params = {0.15, 0.05, 0.06, 0.2, 0, 1.0};
     gait_gains = {120,1,140,1};
     PrintGaitParams();
 }
 
 void ExecuteFlip() {
-    const float prep_time = 0.4f; // Duration before jumping [s]
+    const float prep_time = 0.6f; // Duration before jumping [s]
     const float launch_time = 0.1f; // Duration before retracting the leg [s]
-    const float fall_time = 2.0f; //Duration after retracting leg to go back to normal behavior [s]
+
+    struct LegGain rear_gains = {120,1,80,1};
+    float rear_up_amp = 0.75f * 0.06f;
+    float rear_down_amp = 0.07f;
 
     float t = millis()/1000.0f - flip_start_time_;
     float pitch = global_debug_values.imu.pitch;
 
     float theta=0,gamma=0;
     if (t < prep_time) {
-        float y = gait_params.stance_height;
+        float y = gait_params.stance_height - rear_up_amp;
         CartesianToThetaGamma(0.0, y, 1, theta, gamma);
-        odrv1Interface.SetCoupledPosition(pitch, gamma, gait_gains);
-        odrv2Interface.SetCoupledPosition(-pitch, gamma, gait_gains);
+        odrv1Interface.SetCoupledPosition(pitch, gamma, rear_gains);
+        odrv2Interface.SetCoupledPosition(-pitch, gamma, rear_gains);
 
         y = gait_params.stance_height - gait_params.up_amp;
         CartesianToThetaGamma(0.0, y, -1, theta, gamma);
@@ -50,18 +53,29 @@ void ExecuteFlip() {
         CartesianToThetaGamma(0.0, y, 1, theta, gamma);
         odrv3Interface.SetCoupledPosition(-pitch, gamma, gait_gains);
     } else if (t >= prep_time && t < prep_time + launch_time) {
-        float y = gait_params.stance_height;
+        float y = gait_params.stance_height - rear_up_amp;
         CartesianToThetaGamma(0.0, y, 1, theta, gamma);
-        odrv1Interface.SetCoupledPosition(pitch, gamma, gait_gains);
-        odrv2Interface.SetCoupledPosition(-pitch, gamma, gait_gains);
+        odrv1Interface.SetCoupledPosition(pitch, gamma, rear_gains);
+        odrv2Interface.SetCoupledPosition(-pitch, gamma, rear_gains);
 
         y = gait_params.stance_height + gait_params.down_amp;
         CartesianToThetaGamma(0.0, y, -1, theta, gamma);
         odrv0Interface.SetCoupledPosition(pitch, gamma, gait_gains);
         CartesianToThetaGamma(0.0, y, 1, theta, gamma);
         odrv3Interface.SetCoupledPosition(-pitch, gamma, gait_gains);
-    } else if (t >= prep_time + launch_time && t < prep_time + launch_time + fall_time) {
-        float y = gait_params.stance_height;
+    } else if (pitch < 85.0*M_PI/180.0) {
+        float y = gait_params.stance_height - rear_up_amp;
+        CartesianToThetaGamma(0.0, y, 1, theta, gamma);
+        odrv1Interface.SetCoupledPosition(pitch, gamma, rear_gains);
+        odrv2Interface.SetCoupledPosition(-pitch, gamma, rear_gains);
+
+        y = gait_params.stance_height;
+        CartesianToThetaGamma(0.0, y, -1, theta, gamma);
+        odrv0Interface.SetCoupledPosition(-pitch, gamma, gait_gains);
+        CartesianToThetaGamma(0.0, y, 1, theta, gamma);
+        odrv3Interface.SetCoupledPosition(pitch, gamma, gait_gains);
+    } else {
+        float y = gait_params.stance_height + rear_down_amp;
         CartesianToThetaGamma(0.0, y, 1, theta, gamma);
         odrv1Interface.SetCoupledPosition(pitch, gamma, gait_gains);
         odrv2Interface.SetCoupledPosition(-pitch, gamma, gait_gains);
